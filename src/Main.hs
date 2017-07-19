22bc756ada62ec2c056e66a9dac7772633596e4c
Jeffrey Rosenbluth <jeffreyrosenbluth@Jeffreys-MacBook-Pro.local>
2017-07-18 21:58:09 -0600

    parser

--- src/Main.hs
+++ src/Main.hs
@@ -1,12 +1,14 @@
 module Main where
 
-import Syntax
-import Lexer
-import Parser
+import           Syntax
+import           Lexer
+import           Parser
 
+import           Data.List             (isSuffixOf)
+import qualified Data.Text.Lazy.IO     as T
+import           System.Environment    (getArgs)
+import           System.FilePath.Find
 import           Text.Megaparsec
-import           Data.Text.Lazy
-import           System.Environment       (getArgs)
 
 main ::IO ()
 main = do
@@ -14,40 +16,24 @@ main = do
   case args of
     []     -> putStrLn "Error - no source file specified."
     [path] -> do
-      p <- parseFile path
-      case p of
-        Left e -> print e
-        Right t -> print t
+      files <- find (depth <? 10) (extension ==? ".jack") path
+      outB  <- traverse parseFile files
+      print outB
 
 
 
 parseFile :: FilePath -> IO (Either (ParseError Char Dec) Class)
 parseFile fp = do
-  f <- readFile fp
-  return $ parse (contents parseClass) "<stdin>" (pack f)
+  f <- T.readFile fp
+  return $ parse (contents parseClass) "<stdin>" f
 
--- main :: IO ()
--- main = do
---   args <- getArgs
---   case args of
---     []     -> putStrLn "Error - no source file specified."
---     [path] -> do
---       files <- find (depth <? 10) (extension ==? ".vm") path
---       outB <- traverse processFile files
---       let out = T.toLazyText . mconcat $ bootstrp : outB
---       T.writeFile (vm2asm path) out
---     _  -> putStrLn "Error - too many command line arguments."
---
--- vm2asm :: FilePath -> FilePath
--- vm2asm path = if isSuffixOf ".vm" path
---                 then reverse . ("msa" ++) . drop 2 $ reverse path
---                 else makeFilename path
---
--- dropvm :: FilePath -> Builder
--- dropvm = T.fromString . reverse . takeWhile (/= '/') . drop 3 . reverse
---
--- makeFilename :: FilePath -> String
--- makeFilename path = path ++
---                   ( reverse . ("msa." ++) . takeWhile (/= '/') . drop 1 . reverse
---                   $ path
---                   )
+vm2asm :: FilePath -> FilePath
+vm2asm path = if isSuffixOf ".vm" path
+                then reverse . ("msa" ++) . drop 2 $ reverse path
+                else makeFilename path
+
+makeFilename :: FilePath -> String
+makeFilename path = path ++
+                  ( reverse . ("msa." ++) . takeWhile (/= '/') . drop 1 . reverse
+                  $ path
+                  )
